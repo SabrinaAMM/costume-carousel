3db2141c23767124b903b62b6e71bcbf8c799988
Philipp Schiessler <philipp.schiessler@mail.de>
2020-11-17 12:54:18 +0100

    created bookings story

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,51 @@
+class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    find_costume
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.costume = @costume
+    if @booking.save
+      redirect_to dashboard_index_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    find_costume
+    find_booking
+  end
+
+  def update
+    find_booking
+    if @booking.update(booking_params)
+      redirect_to dashboard_index_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    find_booking
+    @booking.destroy
+    redirect_to dashboard_index_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
+  end
+
+  def find_costume
+    @costume = Costume.find(params[:costume_id])
+  end
+
+  def find_booking
+    @booking = Booking.find(params[:id])
+  end
+end
