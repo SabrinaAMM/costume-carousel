6f0eda7cda6278fdb31a98dc1952d317d03778b2
Mario Stehling <mario.stehling@gmx.de>
2020-11-17 11:34:11 +0100

    Added costume/new+create story

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -6,4 +6,21 @@ class CostumesController < ApplicationController
   def show
     @costume = Costume.find(params[:id])
   end
+
+  def new
+    @costume = Costume.new
+  end
+
+  def create
+    @costume = Costume.new(costume_params)
+    @costume.user = current_user
+    @costume.save
+    redirect_to costume_path(@costume)
+  end
+
+  private
+
+  def costume_params
+    params.require(:costume).permit(:name, :size, :price_per_day, :category, :description)
+  end
 end
