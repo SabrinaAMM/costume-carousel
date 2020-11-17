54fb51e210cef409bfad52100fdb1e851424e458
Philipp Schiessler <philipp.schiessler@mail.de>
2020-11-17 19:49:10 +0100

    changed dashboard view for showing received bookings

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,12 @@
 class DashboardController < ApplicationController
   def index
     @user = current_user
+    # @user_costumes = @user.costumes
+    # @user_costume_ids = []
+    # @user_costumes.each do |costume|
+    #   @user_costume_ids << costume.id
+    # end
+
+    # @received_bookings = Booking.where(@user_costume_ids.include(costume_id))
   end
 end
