5c903b21e7edffda4079cb3788afbb64293acfee
Mario Stehling <mario.stehling@gmx.de>
2020-11-18 12:17:41 +0100

    Added navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 User.create!(email: "rentee@example.com", password: "123456", lender: false)
 User.create!(email: "lender@example.com", password: "123456", lender: true)
-#Costume.create!(name: "Cost1", size: "XL", price_per_day: 5.15, category: "Carneval", description: "Great costume", user_id: 2)
-#Costume.create!(name: "Cost2", size: "S", price_per_day: 5.15, category: "Scary", description: "Great costume !!", user_id: 2)
-#Booking.create!(user_id: 2, costume_id: 1)
-#Booking.create!(user_id: 2, costume_id: 1)
+Costume.create!(name: "Cost1", size: "XL", price_per_day: 5.15, category: "Carneval", description: "Great costume", user_id: 2)
+Costume.create!(name: "Cost2", size: "S", price_per_day: 5.15, category: "Scary", description: "Great costume !!", user_id: 2)
+Booking.create!(user_id: 2, costume_id: 1)
+Booking.create!(user_id: 2, costume_id: 1)
