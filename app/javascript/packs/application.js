e1529ac5d4450355ff171a119c6dccfd2ddf8808
Sabrina Ammerl <michaela.ammerl@gmx.de>
2020-11-20 13:06:04 +0100

    added flatpickr calender

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,6 +31,8 @@ require("channels")
 import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
+import { initFlatpickr} from '../plugins/init_flatpickr';
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -39,6 +41,7 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   initMapbox();
   initAutocomplete();
+  initFlatpickr();
 })
 
 
--- app/models/costume.rb
+++ app/models/costume.rb
@@ -5,4 +5,11 @@ class Costume < ApplicationRecord
   validates :name, :size, :price_per_day, :category, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+ 
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
